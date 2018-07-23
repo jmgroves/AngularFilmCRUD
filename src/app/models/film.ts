cd198b8db805b0e00f0c7fac1c269f5095f2462a
jmgroves <jared.m.groves@gmail.com>
2018-07-23 08:34:02 -0600

    close but no cigar

--- src/app/models/film.ts
+++ src/app/models/film.ts
@@ -1,37 +1,37 @@
 import { Actor } from '../models/actor';
 
 export class Film {
-  private id: number;
-  private title: string;
-  private description: string;
-  private releaseYear: number;
-  private rentalRate: number;
-  private length: number;
-  private rating: string;
-  private languageId: number;
-  private actors: Array<Actor>;
+  id: number;
+  title: string;
+  description: string;
+  releaseYear: number;
+  rentalRate: number;
+  length: number;
+  rating: string;
+  languageId: number;
+  actors: Array<Actor>;
 
 
   constructor(
-    $id?: number,
-    $title?: string,
-    $description?: string,
-    $releaseYear?: number,
-    $rentalRate?: number,
-    $length?: number,
-    $rating?: string,
-    $languageId?: number,
-    $actors?: Array<Actor>
+    id?: number,
+    title?: string,
+    description?: string,
+    releaseYear?: number,
+    rentalRate?: number,
+    length?: number,
+    rating?: string,
+    languageId?: number,
+    actors?: Array<Actor>
   ) {
-    this.id = $id;
-    this.title = $title;
-    this.description = $description;
-    this.releaseYear = $releaseYear;
-    this.rentalRate = $rentalRate;
-    this.length = $length;
-    this.rating = $rating;
-    this.languageId = $languageId;
-    this.actors = $actors;
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.releaseYear = releaseYear;
+    this.rentalRate = rentalRate;
+    this.length = length;
+    this.rating = rating;
+    this.languageId = languageId;
+    this.actors = actors;
   }
 
   /**
